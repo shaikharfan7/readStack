08016376f5c25e2bbc84ddb5ae3294c553fc6c1d
Arfan Shaikh <shaikharfan7@gmail.com>
2020-10-18 12:10:19 +0530

    Fixed Spinner Alignment & Added Favicon

--- js/index.js
+++ js/index.js
@@ -1,16 +1,3 @@
-var arr=[];
-var myVar;
-
-function loadScreen(){
-  myVar = setTimeout(showPage, 3000);
-}
-
-function showPage() {
-  document.getElementById("loader").style.display = "none";
-  document.getElementById("mainCardContainer").style.display = "block";
-}
-
-
 fetch('https://script.google.com/macros/s/AKfycbzRtjjLe2Gdh9VbTjQCpJ9PHKB-ojfLuR_4Bm6fOLhyWd_gMjA/exec')
     .then(res => res.json())
     .then((out) => {
@@ -23,11 +10,6 @@ fetch('https://script.google.com/macros/s/AKfycbzRtjjLe2Gdh9VbTjQCpJ9PHKB-ojfLuR
             y.appendChild(t);
             document.getElementById("cardList").appendChild(link).appendChild(y);
         }
-        //arr=out;
-        //for (var i = 3 - 1; i >= 0; i--) {
-        //document.getElementById('mainCardContainer').innerHTML = "<div id='card' onclick='assignLink()'><div/><br><br>";
-        //document.getElementById('card').innerHTML=out.user[i].title + "<br> By " + out.user[i].author;
-        //console.log(out.user[i].title + " By " + out.user[i].author)
-      //}
+        Spinner.hide();
 }).catch(err => console.error(err));
 
